80212f30f25fa2f1968797862445e75c3a44c783
dak151449 <dak151449@gmail.com>
2023-11-14 17:01:52 +0300

    (#1) Добавлена работа с конфиг файлом

--- main.cpp
+++ main.cpp
@@ -1,65 +1,60 @@
 #include <iostream>
 #include <unistd.h>
 #include <set>
+#include <regex>
 #include <map>
 #include "Api.h"
 #include "LegacyDependencyAnalyzer.h"
 #include "PatchMaker.h"
 #include "Cacher.h"
+#include "Config.h"
+
 
 using namespace std;
 
 // А точно безоавсно хранить админский ключ в строке на гите, и так сойдет ...
-std::string postgreConnStr = "user=doadmin password=AVNS_xMD70wwF41Btbfo6iaz host=db-postgresql-fra1-79796-do-user-14432859-0.b.db.ondigitalocean.com port=25060 dbname=test target_session_attrs=read-write";
-//std::string postgreConnStr = "user=edgar password=genius host=host.docker.internal port=5432 dbname=test target_session_attrs=read-write";
+std::string postgreConnStr = "";
 //Прокся
 //std::string apiURL = "http://64.226.73.174:8080";
 //не прокся (медленно)
 std::string apiURL = "https://rdb.altlinux.org";
-int threadsSize = 100;
 
-std::set<std::string> errorPackages;
-
-
+// возвращает true если пакет подходит хотя бы под одно регулярное выражение из конфига
+bool packageExamined(std::string pack, const std::vector<std::string>& pack_regexs) {
+    for (auto r: pack_regexs) {
+        std::regex R(r);
+        std::smatch cm;
+        if (std::regex_search(pack, cm, R)) {
+            return true;
+        }
+    }
+    return false;
+}
 
 int main(int argc, char *argv[]) {
+    Config cf;
+    postgreConnStr = cf.getConnectDB();
+    auto pack_regexs = cf.getPackRegexs();
 
-    // Api::getBranchPackageNames("sisyphus");
+    std::cout << postgreConnStr << std::endl;
     Cacher CH;
 
     auto  L = LegacyDependencyAnalyzer();
     
     auto t = Api::getBranchPackageNames("sisyphus");
-    // t.resize(10);
     set<std::string> test;
     int index = 0;
     for (auto it = t.begin(); it != t.end(); it++) {
-        // if (index <= 20) {
-        //     index++;
-        //     continue;
-        // } 
-        // if (index >= 40) {
-        //     break;
-        // }
-        // if (*it != "asterisk-core-sounds-fr-gsm" && *it != "asterisk-core-sounds-fr-siren14" &&
-            // *it != "ocaml-sqlite3" && *it != "asterisk-core-sounds-fr-sln16" &&
-            // *it != "samba-client-control" && *it != "alterator-wizardface" && 
-        if(
-            (*it).substr(0, string("asterisk-core-sounds").size()) != "asterisk-core-sounds") {
+        if (!packageExamined(*it, pack_regexs)) {
             continue;
         }
+
         test.insert(*it);
         index++;
     }
 
     L.analysingBranchPackages(test);
-    // L.packagesToAnalyse = t;
-
-    //  for(auto pack: L.packagesToAnalyse) {
-    //     cout << pack.second.first << " Release: " << pack.second.second << endl;
-    // }
-
-    // return 0;
+ 
 
     std::vector<std::string> packages;
     std::map<std::string, std::pair<std::string, std::string>> test_pack;
